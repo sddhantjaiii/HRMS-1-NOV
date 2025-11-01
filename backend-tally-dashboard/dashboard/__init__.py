9455e56aa8a6a7313a3adf48da766fbe2531bd7b
sddhantjaiii <122254271+sddhantjaiii@users.noreply.github.com>
2025-11-01 18:33:55 +0530

    first commit-2

--- backend-tally-dashboard/dashboard/__init__.py
+++ backend-tally-dashboard/dashboard/__init__.py
@@ -1,6 +1,4 @@
-# This will make sure the app is always imported when
-# Django starts so that shared_task will use this app.
-from .celery import app as celery_app
+# Celery is not used in this deployment
+# Using thread-based background tasks instead
 
-__all__ = ('celery_app',)
 
